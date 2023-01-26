a98ad6c33c7d4e1c8ff91eb29fc3de2397002ed8
dongbohu <dongbo.hu@gmail.com>
2023-01-26 17:11:32 -0500

    update

--- rename_histology.py
+++ rename_histology.py
@@ -7,25 +7,22 @@ is consistent with the filesystem hierarchy required by cloud storage.
 This script is based on:
 https://github.com/faryabiLab/hpap-apps/blob/master/data_curator_tools/psv_pipelines/psv_histology_upload.py
 
-TO-DO:
-------
-* Replace `pandas` module (which is harder to understand) with a simpler
-  Python module (such as `openpyxl`) when reading Excel file and matching
-  filenames.
-
+The original version uses Pandas to parse the Excel file and match image
+files. It was an overkill and made the code harder to understand, so
+`pandas` was replaced by `openpyxl` when Excel file was read in.
 """
 
-import datetime
 import os
-import pandas as pd
 import re
 import shutil
 import sys
 import time
 
+import openpyxl
+import pandas as pd # dhu: delete it later
+
 IMG_FILE_EXTENSION = '.ndpi' # extension of image files
 
-pd.options.mode.chained_assignment = None
 unique_dict = dict()
 
 anatomy_dict = {
@@ -67,6 +64,70 @@ def my_log(message, with_time=True):
     print(message, flush=True)
 
 
+def get_filename_key(input_filename, rm_extension):
+    """
+    Return a filename key, which includes only '_' and alphanumric in
+    `input_filename`. If `rm_extension` is True, the extension will be
+    also removed from the key before it's returned.
+    """
+
+    if rm_extension:
+       input_filename = input_filename.rsplit('.', 1)[0]
+
+    filename_key = ""
+    for c in input_filename:
+        if c == '_' or c == '-' or c.isalpha() or c.isdigit():
+            filename_key += c
+
+    if len(filename_key) == 0 or not filename_key.startswith('HPAP'):
+        my_log("ERROR: invalid filename '{input_filename}'")
+        sys.exit(1)
+
+    return filename_key
+
+
+def get_donor_id(input_str):
+    """
+    Return the donor ID from input_str.
+    (assume thqat `input_str` is in the format of "HPAP xxx_whatever".
+    """
+
+    hpap_str = input_str.split('_', 1)[0]
+    num_part = hpap_str.split('HPAP')[1]
+    donor_id = num_part.zfill(3)
+
+    return donor_id
+
+
+def check_img_filenames(filenames):
+    """
+    Check input image filenames.
+    If any of them has different HPAP donor ID, print an error and exit
+    immediately.
+    If everything is good, return the donor_id and a dict (whose key is
+    normalized filename key, value is the actual image filename).
+    """
+
+    donor_id = None
+    fn_map = dict()
+    for fn in filenames:
+        fn_key = get_filename_key(fn, rm_extension=True)
+        current_donor_id = get_donor_id(fn_key)
+        if donor_id is None:
+            donor_id = current_donor_id
+        elif donor_id != current_donor_id:
+            print(f"ERROR: inconsistent donor ID in '{fn}'")
+            sys.exit(1)
+
+        if fn_key in fn_map:
+            print(f"ERROR: duplicate filename key in '{fn}'")
+            sys.exit(1)
+
+        fn_map[fn_key] = fn
+
+    return donor_id, fn_map
+
+
 def check_src(src_dir):
     """Ensure that source data directory is in good shape."""
 
@@ -96,12 +157,13 @@ def check_src(src_dir):
         my_log(f"ERROR: no image file found in '{src_dir}'")
         sys.exit(1)
 
-    # dhu: Ensure that image filenames have identical donor IDs
+    # Ensure that image filenames have identical donor IDs
+    donor_id, fn_map = check_img_filenames(img_files)
 
-    my_log(f"{num_img} image file(s) found in '{src_dir}'")
-
+    print(f"{num_img} image file(s) found in '{src_dir}'")
     excel_filename = f"{src_dir}/{excel_files[0]}"
-    return img_files, excel_filename
+
+    return donor_id, fn_map, excel_filename
 
 
 def check_dest(dest_dir):
@@ -122,111 +184,171 @@ def check_dest(dest_dir):
         sys.exit(4)
 
 
-def is_date_str(input_str):
-    if re.findall('\\d{2,4}-\\d{1,2}-\\d{1,2}', input_str):
-        return True
-
-    if re.findall('\\d{1,2}/\\d{1,2}/\\d{2,4}', input_str):
-        return True
-
-    return False
-
-def format_histology_df(df):
-    df_clone = df.copy()
-
-    # If the first row is header line, skip it.
-    if 'prep' in [str(x).lower() for x in df_clone.iloc[0, :].values.tolist()]:
-        df_clone = df_clone.iloc[1:, :]
-
-    # A map whose key is Excel column name, and value is new column name
-    col_dict = dict()
-
-    for col in df_clone.columns:
-        col_vals = df_clone[col].values.astype(str).tolist()
-        first_val = col_vals[0]
-        first_lower = first_val.lower()
-        if 'HPAP' in first_val:
-            col_dict[col] = 'raw_info'
-        elif 'oct' in first_lower or 'ffpe' in first_lower or 'vand' in first_lower:
-            col_dict[col] = 'stain'
-        elif first_val == '6489':
-            df_clone.drop(col, axis=1, inplace=True)
-        elif is_date_str(first_val):
-            # Skip date columns (such as "Captured Date" and "Upload Date")
-            df_clone.drop(col, axis=1, inplace=True)
-        elif 'nan' not in first_val:
-            int_first = int(float(first_val))
-            if int_first > 10000:
-                col_dict[col] = 'img_id'
-                if 'no tissue' in df_clone[col].values:
-                    drop_idx = df_clone.index[df_clone[col] == 'no tissue'].tolist()
-                    df_clone.drop(drop_idx, axis=0, inplace=True)
-            elif int_first == 1 or int_first == 426:
-                df_clone.drop(col, axis=1, inplace=True)
-        elif 'nan' in first_val:
-            df_clone.drop(col, axis=1, inplace=True)
-
-    df_clone.rename(columns=col_dict, inplace=True)
-
-    # Since October 2021, all values in "Image ID" column of Excel file
-    # are blank. In order to keep them compatible with the old format,
-    # all blank image IDs are set to 'NA'.
-    if 'img_id' not in df_clone.columns:
-        df_clone['img_id'] = 'NA'
-
-    # Drop rows whose value in 'raw_info' column is missing
-    dropped_idx = df_clone[df_clone['raw_info'].isna()].index
-    df_clone.drop(dropped_idx, inplace=True)
-
-    # Drop rows with null image id values
-    dropped_idx = df_clone[df_clone['img_id'].isnull()].index
-    df_clone.drop(dropped_idx, inplace=True)
-
-    return df_clone
-
-
-def get_filename_key(input_str):
+def check_excel_filenames(rows, excel_filename):
     """
-    Return a filename key, which includes only '_' and digits in `input_str`.
+    Ensure that each row in Excel includes same donor ID. If it does,
+    return this donor ID.
     """
 
-    filename_key = ""
-    for c in input_str:
-        if c == '_' or c.isdigit():
-            filename_key += c
+    donor_id = None
+    for r, v in rows.items():
+        current_donor_id = get_donor_id(r)
+        if donor_id is None:
+            donor_id = current_donor_id
+        elif donor_id != current_donor_id:
+            excel_val = v['filename_stem']
+            print("ERROR: inconsistent donor ID in '{excel_val}' of '{excel_filename}'")
+            sys.exit(1)
 
-    if len(filename_key) == 0:
-        my_log("ERROR: filename key not found in '{input_str}'")
+    return donor_id
+
+
+def map_excel_columns(sheet_obj):
+    """
+    Read the first data row (not the one with column names) of input
+    Excel sheet object and return a map between column number in Excel
+    sheet and proper column name (which is NOT related to the column
+    name in Excel).
+    """
+
+    max_col = sheet_obj.max_column
+    col_num2name = dict()
+
+    for c in range(1, max_col + 1):
+        cell_obj = sheet_obj.cell(row=2, column=c)
+        cell_val = cell_obj.value
+
+        # If a column's value is empty or not a string, skip it.
+        if cell_val is None or not isinstance(cell_val, str):
+            continue
+
+        cell_val = cell_val.strip()
+        lower_val = cell_val.lower()
+
+        # Skip columns that includes whitespace characters only
+        if len(lower_val) == 0:
+            continue
+
+        if 'HPAP' in cell_val:
+            col_num2name[c] = 'filename_stem'
+            continue
+
+        if 'oct' in lower_val or 'ffpe' in lower_val or 'vand' in lower_val:
+            col_num2name[c] = 'stain'
+            continue
+
+    return col_num2name
+
+
+def read_excel(filename, donor_id):
+    """
+    Read Excel spreadsheet, parse the data in each cell, and return a
+    dict, whose key is a "normalized" filename key, and whose value is
+    another dict of other columns.
+    """
+
+    # workbook and sheet objects
+    wb_obj = openpyxl.load_workbook(filename, read_only=True)
+    sheet_obj = wb_obj.active
+
+    col_num2name = map_excel_columns(sheet_obj)
+
+    # If `filename_stem` column is not found, print out an error and exit.
+    if 'filename_stem' not in col_num2name.values():
+        print(f"ERROR: HPAP column not found in '{filename}'")
         sys.exit(1)
 
-    return filename_key
+    max_row = sheet_obj.max_row
+    max_col = sheet_obj.max_column
 
+    # Read the whole Excel file and create a dict, whose key is the value
+    # of `filename_stem`, and whose value is another dict of other columns.
+    rows = dict()
+    for r in range(2, max_row + 1):
+        current_row = dict()
+        for c in range(1, max_col + 1):
+            if c not in col_num2name:
+                continue
+            col_name = col_num2name[c]
+            cell_val = sheet_obj.cell(row=r, column=c).value.strip()
+            current_row[col_name] = cell_val
 
-def get_polished_info(input_str):
-    polished_str = input_str.strip()
+        filename_stem = current_row.get('filename_stem', '')
+        row_key = get_filename_key(filename_stem, rm_extension=False)
+        if len(row_key) == 0 or not row_key.startswith('HPAP'):
+            print(f"ERROR: invalid HPAP value on row #{r} of '{filename_stem}'")
+            sys.exit(1)
 
-    if polished_str.startswith('HPAP '):
-        polished_str = input_str.replace('HPAP ', 'HPAP')
+        if row_key in rows:
+            print(f"ERROR: duplicate HPAP value on row #{r} of '{filename_stem}'")
+            sys.exit(1)
 
-    if '_' not in polished_str or 'HPAP' not in polished_str:
-        print("ERROR: invalid filename in Excel: '{input_str}'")
+        rows[row_key] = current_row
+
+    excel_donor_id = check_excel_filenames(rows, filename)
+    if excel_donor_id != donor_id:
+        print(
+            f"ERROR: donor ID in Excel ({excel_donor_id}) does not match "
+            f"the one in image files ({donor_id})"
+        )
         sys.exit(1)
 
-    tokens = polished_str.split("_", 1)
-    donor = tokens[0].strip()
-    donor_id = donor.split("HPAP")[1].zfill(3)
-
-    return f"HPAP{donor_id}_{tokens[1]}"
+    return rows
 
 
-def parse_donor(input_str):
-    donor = input_str.replace("HPPAP", 'HPAP').split(" ", 1)[0].strip().rsplit("_")[0]
-    padded_id = donor.split("HPAP")[-1].zfill(3)
+def match_excel_to_images(excel_dict, img_dict):
+    """
+    Match the data in Excel with actual image files.
+    If everything is good, return a another dict maps each image file to
+    a new filename (which will be saved in the destination directory.
+    """
 
-    return f"HPAP-{padded_id}"
+    # Ensure that each image file matches one row in Excel spreadsheet.
+    for ik, iv in img_dict.items():
+        if ik not in excel_dict():
+            print(f"ERROR: image file '{iv}' not match any row in Excel")
+            sys.exit(1)
+
+    # Ensure that each row in Excel matches one image file
+    src2dest = dict()
+    dest_key_counter = dict()
+    for xk, xv in excel_dict.items():
+        if xk not in img_dict():
+            cell_v = xv['filename_stem']
+            print(f"ERROR: '{cell_v}' not match any image files")
+            sys.exit(1)
+
+        anatomy = get_anatomy(xk)
+        short_anatomy = get_short_anatomy(anatomy)
+        excel_stain = xv['stain']
+        new_stain = rename_stain(excel_stain)
+        anatomy = anatomy.replace(" ", "-").str.replace("---", "-")
+        dest_key = "_".join(
+            [
+                f"HPAP-{donor_id}",
+                "Histology",
+                anatomy,
+                new_stain,
+                "H-and-E",
+            ]
+        )
+
+        counter = dest_key_counter.get(dest_key, 0)
+        uniq_num = counter + 1
+        dest_key_counter[dest_key] = uniq_num
+
+        dest_name = f"{dest_key}_{uniq_num}{IMG_FILE_EXTENSION}"
+        src_img_filename = img_dict[xk]
+
+        src2dest[src_img_filename] = {
+            'dir': short_anatomy,
+            'filename': dest_name,
+        }
+
+        return src2dest
 
 
-def parse_anatomy(value):
+def get_anatomy(value):
     pan = re.findall("(pancreas)\\s?-?\\s?(\\w+)", value)
     spl = re.findall("(spleen)", value)
     lyn = re.findall("(LN)\\s?-?\\s?(\\w+)?", value)
@@ -378,12 +500,8 @@ def map_src_to_dest(src_dir, img_files, excel_filename):
     to the cloud storage.
     """
 
-    # Read the Excel file
-    my_log("Reading Excel file ...")
-    raw_histology = pd.read_excel(io=excel_filename, header=None)
-
-    # Polish the data in Excel file
-    clean_histology = format_histology_df(raw_histology)
+    # Read Excel file
+    excel_data = read_excel(excel_filename)
 
     # Add new columns to match each row in Excel with an image file:
     clean_histology['filename_key'] = clean_histology['raw_info'].apply(
@@ -491,7 +609,20 @@ if __name__ == "__main__":
     dest_dir = args[2]
 
     # Make sure that source directory is good
-    img_files, excel_filename = check_src(src_dir)
+    donor_id, fn_map, excel_filename = check_src(src_dir)
+
+    rows = read_excel(excel_filename, donor_id)
+
+    #import json; print(json.dumps(rows, indent=2)); sys.exit(0)  # dhu test
+    for x in fn_map:
+        if x not in rows:
+            print(x)
+
+    for x in rows:
+        if x not in fn_map:
+            print(x)
+
+    sys.exit(0)
 
     # Make sure that destination directory is good:
     check_dest(dest_dir)
